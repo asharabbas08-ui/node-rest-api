295f1e45108ede524dfe5c31e3653104a1f386ac
Ashar <asharaabbas08@gmail.com>
2026-04-17 20:58:48 +0530

    Added root route

--- server.js
+++ server.js
@@ -47,3 +47,16 @@ app.delete('/users/:id', (req, res) => {
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+  res.send("API is working 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
