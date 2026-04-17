4d36e29e0743e94eb585db98898ed1f12f8d76cd
Ashar <asharaabbas08@gmail.com>
2026-04-17 18:17:19 +0530

    Initial commit - REST API

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Dummy data (starting users)
+let users = [
+    { id: 1, name: "Ashar" },
+    { id: 2, name: "Ali" }
+];
+
+// GET → sab users dikhaye
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+// POST → naya user add kare
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name
+    };
+    users.push(newUser);
+    res.json(newUser);
+});
+
+// PUT → user update kare
+app.put('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+
+    if (user) {
+        user.name = req.body.name;
+        res.json(user);
+    } else {
+        res.send("User not found");
+    }
+});
+
+// DELETE → user delete kare
+app.delete('/users/:id', (req, res) => {
+    users = users.filter(u => u.id !== parseInt(req.params.id));
+    res.send("User deleted");
+});
+
+// Server start
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
